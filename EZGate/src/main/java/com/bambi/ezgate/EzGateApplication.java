6a9fa5e75b6cad9c66cf4099e0525e192d7687db
RichardVicentMoose <44000760+RichardReindeer@users.noreply.github.com>
2023-03-01 10:48:02 +0800

    【Build】
    项目构建

---
+++ EZGate/src/main/java/com/bambi/ezgate/EzGateApplication.java
@@ -0,0 +1,13 @@
+package com.bambi.ezgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzGateApplication.class, args);
+    }
+
+}
---
+++ EZGate/src/test/java/com/bambi/ezgate/EzGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bambi.ezgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
